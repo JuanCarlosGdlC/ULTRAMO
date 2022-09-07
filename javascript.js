07cc019f3ad72425cd23db72878fac8930ce7ce3
JuanCarlosGdlC <juancarlosgarciadeleoncarrillo@gmail.com>
2022-09-07 17:57:32 -0500

    Primer Archivo

---
+++ javascript.js
@@ -0,0 +1,54 @@
+let pedir1 = document.getElementById("btn1");
+let pedir2 = document.getElementById("btn2");
+let pedir3 = document.getElementById("btn3");
+let pedir4 = document.getElementById("btn4");
+let pedir5 = document.getElementById("btn5");
+let pedir6 = document.getElementById("btn6");
+let pedir7 = document.getElementById("btn7");
+let pedir8 = document.getElementById("btn8");
+let pedir9 = document.getElementById("btn9");
+let pedir10 = document.getElementById("btn10");
+
+pedir1.addEventListener("click", function(){
+confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+});
+
+pedir2.addEventListener("click", function(){
+confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+ });
+
+pedir3.addEventListener("click", function(){
+ confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+ });
+
+pedir4.addEventListener("click", function(){
+confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+ });
+
+ pedir5.addEventListener("click", function(){
+    confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+     });
+
+     pedir6.addEventListener("click", function(){
+        confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+         });
+
+         pedir7.addEventListener("click", function(){
+            confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+             });
+
+             pedir8.addEventListener("click", function(){
+                confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+                 });
+
+                 pedir9.addEventListener("click", function(){
+                    confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+                     });
+
+                     pedir10.addEventListener("click", function(){
+                        confirm("Tu pedido va ser procesado, Gracias por comprar con nostros")
+                         });
+
+let description1 = document.getElementById("");
+
+console.log("Mensaje de Conexion");
